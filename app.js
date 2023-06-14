687f0bd5081c56d2155abe5c43833c9b33d6df55
SRubiRosales <sharon_rosales98@hotmail.com>
2023-06-14 11:49:22 -0600

    Introduction to typescript

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+const message = 'Hola mundo';
+const hero = {
+    name: 'Ironman',
+    age: 45
+};
+console.log(message);
+console.log(hero.age + 1);
---
+++ app.ts
@@ -0,0 +1,7 @@
+const message: string = 'Hola mundo';
+const hero = {
+    name: 'Ironman',
+    age:45
+}
+console.log(message);
+console.log(hero.age + 1);
