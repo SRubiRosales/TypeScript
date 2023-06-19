3d162b60d62f6e21d4237980618b60a2266c49b6
SRubiRosales <sharon_rosales98@hotmail.com>
2023-06-19 12:01:20 -0600

    Arrays

---
+++ tipos/arrays.ts
@@ -0,0 +1,8 @@
+(() => {
+    //Arreglo que admite numbers
+    const numbers: (number)[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const villian = ['Duende verde', 'Rinho', 'Dr Octopus'];
+    numbers.push(11)
+    console.log(numbers)
+    villian.forEach(v => console.log(v.toUpperCase()))
+})();
