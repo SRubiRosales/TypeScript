b17ac70ca0fd887c40d2d235b1fb449215ecb283
SRubiRosales <sharon_rosales98@hotmail.com>
2023-06-20 12:48:03 -0600

    Void

---
+++ tipos/void.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    //La funcion no retorna nada
+    function callBatman() {
+        return;
+    }
+    const callSuperman = () => {
+    };
+    const a = callBatman();
+    console.log(a);
+})();
---
+++ tipos/void.ts
@@ -0,0 +1,10 @@
+(() => {
+    //La funcion no retorna nada
+    function callBatman():void {
+        return;
+    }
+    const callSuperman = ():void => {
+    }
+    const a = callBatman()
+    console.log(a)
+})()
