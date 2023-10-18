15bed7b02ce99e3636b2a710a27b1a10154fb007
SRubiRosales <sharon_rosales98@hotmail.com>
2023-10-18 11:44:32 -0600

    Var let

---
+++ ES6/let-var.ts
@@ -0,0 +1,12 @@
+(() => {
+    //console.log(a);
+    const nombre:string = 'Sharon';
+    const heroe = {
+        a: 1,
+        b: 2
+    }
+    const getName = ():void => {
+        console.log('Viejo getName')
+    };
+    //getName = () => {console.log('Nuevo getName')};
+})();
