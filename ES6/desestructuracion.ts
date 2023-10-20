795f05ffb5669242b92982fe339929c13004a0bc
SRubiRosales <sharon_rosales98@hotmail.com>
2023-10-20 12:28:25 -0600

    Destructuring

---
+++ ES6/desestructuracion.ts
@@ -0,0 +1,25 @@
+(() => {
+
+    type Avengers = {
+        nick: string;
+        ironman: string;
+        vision: string;
+        activo: boolean;
+        poder: number;
+    }
+    const avengers = {
+        nick: 'Samuel L. Jackson',
+        ironman: 'Robert Downey Jr',
+        vision: 'Paul Bettany',
+        activo: true,
+        poder: 1500.123456
+    }
+
+    // const {poder, vision} = avengers;
+    // console.log(poder.toFixed(2), vision.toUpperCase());
+    const printAvenger = ( { ironman, ...resto}:Avengers ) => {
+        console.log(ironman);
+        console.log(resto);
+    }
+    printAvenger(avengers);
+})();
