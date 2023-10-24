3608b4a7aa7b3a37781a7a24ee59577d2f96c294
SRubiRosales <sharon_rosales98@hotmail.com>
2023-10-24 13:18:44 -0600

    For of

--- main.js
+++ main.js
@@ -29,6 +29,24 @@ var __rest = (this && this.__rest) || function (s, e) {
     console.log(ironman, capitan);
 })();
 (() => {
+    const ironman = {
+        name: 'Ironman',
+        weapon: "Armorsuit"
+    };
+    const captainAmerica = {
+        name: 'Captain América',
+        weapon: "Shield"
+    };
+    const thor = {
+        name: 'Thor',
+        weapon: "Mjolnir"
+    };
+    const avengers = [ironman, thor, captainAmerica];
+    for (const avenger of avengers) {
+        console.log(avenger.name, avenger.weapon);
+    }
+})();
+(() => {
     const nombre = 'Sharon';
     const heroe = {
         a: 1,
