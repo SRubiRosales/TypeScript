54b79b7d6a39702ff2634ad543e09b6ad0bc2751
SRubiRosales <sharon_rosales98@hotmail.com>
2023-10-30 16:47:15 -0600

    Short way to assign properties

--- classes/basica.ts
+++ classes/basica.ts
@@ -1,17 +1,13 @@
 (() => {
     class Avenger {
-        private name: string;
-        private team: string;
-        public realName?: string;
         static avgAge: number = 35;//Se accede a la propiedad de manera global
-        constructor(name: string, team: string, realName?: string) {
-            this.name = name;
-            this.team = team;
-            this. realName = realName;
-        }
+        constructor(private name: string, 
+            private team: string, 
+            public realName?: string
+            ) {}
     }
 
-    const antman: Avenger = new Avenger('Antman', 'Cap');
+    const antman: Avenger = new Avenger('Antman', 'Cap', 'Scott Lang');
     console.log(antman);
     console.log(Avenger.avgAge);
 })();
