3b60d9970b0ea3ca46146db337411a1234afc5b2
SRubiRosales <sharon_rosales98@hotmail.com>
2023-11-03 16:09:05 -0600

    Private and public methods

--- classes/basica.ts
+++ classes/basica.ts
@@ -1,13 +1,22 @@
 (() => {
     class Avenger {
         static avgAge: number = 35;//Se accede a la propiedad de manera global
+        static getAvgAge() {
+            return this.name;
+        }
         constructor(private name: string, 
             private team: string, 
             public realName?: string
             ) {}
+
+        public bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
 
     const antman: Avenger = new Avenger('Antman', 'Cap', 'Scott Lang');
     console.log(antman);
     console.log(Avenger.avgAge);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
