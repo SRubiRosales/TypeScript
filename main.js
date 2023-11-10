01cf566e564c80c3a46f9a2bb133cf9119712d15
SRubiRosales <sharon_rosales98@hotmail.com>
2023-11-10 13:52:40 -0600

    Private constructor

--- main.js
+++ main.js
@@ -72,4 +72,25 @@
     const wolverine = new Xmen('Wolverine', 'Logan', true);
     wolverine.fullName = 'Steve';
 })();
+(() => {
+    class Apocalipsis {
+        constructor(name) {
+            this.name = name;
+        }
+        static callApocalipsis() {
+            if (!Apocalipsis.instance) {
+                Apocalipsis.instance = new Apocalipsis('Soy apocalipsis... el unico');
+            }
+            return Apocalipsis.instance;
+        }
+        changeName(newName) {
+            this.name = newName;
+        }
+    }
+    const apocalipsis1 = Apocalipsis.callApocalipsis();
+    const apocalipsis2 = Apocalipsis.callApocalipsis();
+    const apocalipsis3 = Apocalipsis.callApocalipsis();
+    apocalipsis1.changeName('Xavier');
+    console.log(apocalipsis1, apocalipsis2, apocalipsis3);
+})();
 //# sourceMappingURL=main.js.map
