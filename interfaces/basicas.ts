e57f4e0650e8d0688d5d4f6718dc79e349f657d7
SRubiRosales <sharon_rosales98@hotmail.com>
2023-11-13 11:41:50 -0600

    Basic interface

--- interfaces/basicas.ts
+++ interfaces/basicas.ts
@@ -1,18 +1,22 @@
-"use strict";
 (() => {
-    let flash = {
+    interface Hero {
+        name: string;
+        age?: number;
+        powers: number[];
+        getName?: () => string;
+    }
+    let flash: Hero = {
         name: 'Barry Allen',
         age: 24,
         powers: [1, 2]
-    };
-    let superman = {
+    }
+    let superman: Hero = {
         name: 'Clark Kent',
         age: 60,
         powers: [1],
         getName() {
             return this.name;
         },
-    };
+    }
     console.log(flash);
-})();
-//# sourceMappingURL=main.js.map
+})()
