f673e8fb304fdee045cba666937047c520c63fd1
SRubiRosales <sharon_rosales98@hotmail.com>
2023-12-01 11:14:49 -0600

    Namespace

--- main.js
+++ main.js
@@ -1,61 +1,13 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: [1, 2]
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: [1],
-        getName() {
-            return this.name;
-        },
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-    console.log(flash);
-})();
-(() => {
-    class Mutant {
-        constructor(age, name, realName) {
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        mutantPower(id) {
-            return `${this.name} ${this.realName}`;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Sharon',
-        age: 25,
-        address: {
-            id: 124,
-            zip: 'KY2 SUD',
-            city: 'Ottawa'
-        },
-        getFullAddress() {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: 'Melissa',
-        address: {
-            city: 'Toronto',
-            id: 120,
-            zip: 'KY2 SUD'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Sharon'));
+console.log(Validations.validateText('Sha'));
 //# sourceMappingURL=main.js.map
