2b9259af2f5e238179f608826a561b81ad0eae13
SRubiRosales <sharon_rosales98@hotmail.com>
2023-12-15 12:00:28 -0600

    Fix error undefined

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 //import { Hero as SuperHero } from "./classes/hero";
-import * as HeroClasses from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+import { Hero } from "./classes/Hero";
 import power from "./data/powers";
 
-const Hero = 123;
-console.log('Hola Mundo!');
-
-const ironMan = new HeroClasses.Hero('IronMan', 1, 55);
+const ironMan = new Hero('IronMan', 10, 55);
 console.log(ironMan);
-
-console.log(power);
+console.log(ironMan.power)
 
 
