8ef99b8174044bd3ab24357e73481fa0b0a02013
SRubiRosales <sharon_rosales98@hotmail.com>
2024-02-28 13:13:19 -0600

    Clean index content

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { getPost } from "./generics/get-post";
-
-getPost(4).then(
-    post => console.log(post.body.toUpperCase())
-).catch(
-    error => console.error(error)
-).finally(
-    () => console.log('End')
-);
